bcc99b8fed678841b7a1c262a9cb89d4a5ca39a3
Monika <monika.sarafova9111@gmail.com>
2025-01-14 13:47:25 +0200

    creating componenets, modals, using useState and useEffect

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    <footer className="footer">
+      <p>This site is designed to be used for training purposes at SoftUni.</p>
+    </footer>
+}
+
+export default Footer;  
